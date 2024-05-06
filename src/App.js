26da1cb7ddb2fc0c046402941bd8865fc8075124
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-05-07 00:02:10 +0900

    impl: page chift

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemonData, getPokemon } from './utils/pokemon';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [previousURL, setPreviousURL] = useState("");
 
   useEffect(() => {
     const fetchAllPokemonData = async () => {
       let response = await getAllPokemonData(initialURL);
       loadPokemon(response.results);
       setLoading(false);
+      // console.log(response);
+      setNextURL(response.next);
+      setPreviousURL(response.previous);
     };
     fetchAllPokemonData();
 
@@ -28,21 +34,53 @@ function App() {
     setPokemonData(_pokemonData);
   };
 
-  console.log(pokemonData);
+  // console.log(pokemonData);
+
+  const handlePreviousPage = async () => {
+    if (previousURL === null) {
+      return;
+    }
+
+    setLoading(true);
+    const data = await getAllPokemonData(previousURL);
+    await loadPokemon(data.results);
+    setLoading(false);
+    setNextURL(data.next);
+    setPreviousURL(data.previous);
+  };
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemonData(nextURL);
+    // console.log(data);
+    await loadPokemon(data.results);
+    setLoading(false);
+    setNextURL(data.next);
+    setPreviousURL(data.previous);
+  };
 
 
   return (
-    <div className="App">
-      {loading ? (
-        <h1>LOADING...</h1>
-      ) : (
-        <div className="pokemonCardContainer">
-          {pokemonData.map((pokemonName, i) => {
-            return <Card key={i} pokemon={pokemonName} />
-          })}
-        </div>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>LOADING...</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemonName, i) => {
+                return <Card key={i} pokemon={pokemonName} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePreviousPage}>Previous</button>
+              <button onClick={handleNextPage}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
